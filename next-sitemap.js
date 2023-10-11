ba14446696d251bad8c23dfb109348ca8966eb9c
Ash <mohamed.ashraf@birkenstock.com>
2023-10-11 23:31:20 +0400

    updates

--- next-sitemap.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  siteUrl: "https://www.devloom.net",
-  generateRobotsTxt: true,
-  exclude: ["/404"],
-  robotsTxtOptions: {
-    additionalSitemaps: ["https://www.devloom.net/sitemap.xml"],
-  },
-};
