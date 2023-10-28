c9ca5a3f50a4d9e9e74536b67a75ee327d30c831
Ash <mohamed.ashraf@birkenstock.com>
2023-10-29 00:49:56 +0400

    update

--- src/pages/api/validateLicense.js
+++ src/pages/api/validateLicense.js
@@ -9,13 +9,14 @@ import {
   collection,
 } from "firebase/firestore";
 
-// Initialize cors middleware to allow all origins
+// CORS middleware setup for handling cross-origin requests
 const cors = Cors({
-  origin: true,
-  methods: ["POST", "HEAD", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  origin: true, // Allow any origin
+  methods: ["POST", "HEAD", "OPTIONS"], // Allowed methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
 });
 
+// Function to handle middleware asynchronously
 function runMiddleware(req, res, fn) {
   return new Promise((resolve, reject) => {
     fn(req, res, (result) => {
@@ -27,33 +28,40 @@ function runMiddleware(req, res, fn) {
   });
 }
 
+// Main function to validate the license
 const validateLicense = async (req, res) => {
-  // Run the cors middleware
+  // Running CORS middleware to handle cross-origin requests
   await runMiddleware(req, res, cors);
 
+  // Destructuring to get the license key from the request body
   const { licenseKey } = req.body;
+
+  // Using environment variable for product ID
   const GUMROAD_PRODUCT_ID = process.env.NEXT_PUBLIC_GUMROAD_PRODUCT_ID;
 
   try {
-    // Verify with Gumroad API
+    // Make a request to the Gumroad API to verify the license key
     const response = await fetch("https://api.gumroad.com/v2/licenses/verify", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: `product_id=${GUMROAD_PRODUCT_ID}&license_key=${licenseKey}&increment_uses_count=false`,
     });
 
+    // If response isn't successful, throw an error
     if (!response.ok) {
       const text = await response.text();
       throw new Error(`Gumroad error: ${response.status}, ${text}`);
     }
 
+    // Parse the JSON data from the response
     const data = await response.json();
 
+    // If Gumroad API returns success as false, respond with an error
     if (!data.success) {
       return res.status(400).json({ message: "Invalid License Key" });
     }
 
-    // Check Firestore for existing license
+    // Query the Firestore database to check if the license key exists
     const q = query(
       collection(db, "GUMROAD_SALES"),
       where("LicenseKey", "==", licenseKey)
@@ -62,25 +70,25 @@ const validateLicense = async (req, res) => {
     let documentID = null;
     let alreadyActivated = false;
 
+    // Iterating through the documents returned from Firestore
     querySnapshot.forEach((document) => {
       const docData = document.data();
       if (docData.LicenseKey === licenseKey) {
         documentID = document.id;
         if (docData.isValidated) {
-          alreadyActivated = true;
+          alreadyActivated = true; // Check if the license has already been activated
         }
       }
     });
 
+    // Handling the scenarios based on whether the license is already activated or exists in the database
     if (alreadyActivated) {
       return res.status(400).json({
         message: "License is correct but it's been already activated",
       });
     } else if (documentID) {
-      // Update Firestore document
       const documentRef = doc(db, "GUMROAD_SALES", documentID);
       await updateDoc(documentRef, { isValidated: true });
-
       return res.status(200).json({ message: "License Activated" });
     } else {
       return res
