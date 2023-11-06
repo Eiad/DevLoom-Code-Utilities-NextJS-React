96b76b5ce7e896f7da0b0cfd0e0991e8f5cdeb30
Ash <mohamed.ashraf@birkenstock.com>
2023-11-06 15:40:10 +0400

    update

--- src/pages/StringCaseConverter.js
+++ src/pages/StringCaseConverter.js
@@ -12,17 +12,17 @@ function StringCaseConverterPage() {
         </title>
         <meta
           name="description"
-          content="Effortlessly transform text with DevLoom's String Case Converter. Convert to camelCase, PascalCase, snake_case, and more. Ideal for developers, content creators, and anyone needing versatile text formatting."
+          content="Effortlessly transform text with DevLoom&#39;s String Case Converter. Convert to camelCase, PascalCase, snake_case, and more. Ideal for developers, content creators, and anyone needing versatile text formatting."
         />
       </Head>
 
       <div className="top-section">
         <h1>DevLoom String Case Converter - Transform Text with Ease</h1>
         <p>
-          Simplify your text formatting with DevLoom's String Case Converter.
-          Our tool allows you to effortlessly convert strings into various case
-          styles, such as camelCase, PascalCase, snake_case, kebab-case, and
-          more.
+          Simplify your text formatting with DevLoom&#39;s String Case
+          Converter. Our tool allows you to effortlessly convert strings into
+          various case styles, such as camelCase, PascalCase, snake_case,
+          kebab-case, and more.
         </p>
         <p>
           Perfect for developers, content creators, and digital marketers, our
