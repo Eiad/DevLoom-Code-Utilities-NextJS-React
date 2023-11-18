06e20ca657b8232ea3156154e892e3bf54d96157
Ash <mohamed.ashraf@birkenstock.com>
2023-11-18 20:03:39 +0400

    Scrapped JWT as of major production bug making it not working at all

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output Needs to be enabled for electron
+  // output Needs to be enabled for electron's apps build to use out folder for static "Offline" apps
   //output: "export",
 
   // disable the Image Optimization feature during static export
